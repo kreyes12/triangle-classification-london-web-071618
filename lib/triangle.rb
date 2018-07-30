cdeb905b6ac714540f18e0547ac5c37c752cb324
Karla R <karla.y.reyes12@gmail.com>
2018-07-30 10:40:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side_1, :side_2, :side_3
+  
+  @@all = []
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @@all << self
+  end
+  
+  def kind
+    if side_1 == side_2 && side_2 == side_3
+      :equilateral
+    elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  
 end
